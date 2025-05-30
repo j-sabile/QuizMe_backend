753fcbf3372dc7a4a2afc61a3056f50ed25a79ae
j-sabile <jericosabile@gmail.com>
2025-05-30 11:15:38 +0800

    remove openai, fix package.json

--- config.js
+++ config.js
@@ -4,5 +4,4 @@ dotenv.config();
 export const ORIGINS = process.env.ORIGINS.split(",");
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const AWANLLM_API_KEY = process.env.AWANLLM_API_KEY;
